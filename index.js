fef56bb0973c3044df75e3d94abe487cfb3ea0b4
mazdak <nazemi.works@gmail.com>
2019-07-01 23:39:55 -0700

    use fork for cluster

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 //clustering
 //nodemon do not work with clustering
+// it means only for every child have on thread not for whole cluster
+process.env.UV_THREADPOOL_SIZE = 1;
 const cluster = require('cluster');
+//cluster manager is responsible for monitoring health of indi instance
 //cluster manager after forking become false
 // console.log(cluster.isMaster);
 if (cluster.isMaster) {
   //cause index.js executed again in slave mode.
+  //when we call fork node go back and exec index sec time
+  // fork make cluster manager to false
+  // can define thread pool
   cluster.fork();
 
 } else {
-  //i am child.
+  //i am child. slave mode
   const express = require('express');
-
+  const crypto = require('crypto');
   const app = express();
 
+  
+
   app.get('/', (req, res) => {
-    res.send('hi there');
+    crypto.pbkdf2('a','b',100000, 512, 'sha512', ()=> {
+      res.send('hi there');
+    });
+  })
+
+  app.get('/fast', (req, res) => {
+    res.send('hi here');
   })
 
   app.listen(3000);
