4af98c1bacc338666b38fec425f971b22350f877
mazdak <nazemi.works@gmail.com>
2019-07-04 09:57:05 -0700

    test validation of inputs

--- starter/tests/blogs.test.js
+++ starter/tests/blogs.test.js
@@ -1,7 +1,6 @@
 
 const Page = require('./helpers/page');
 
-
 let page;
 beforeEach( async () => {
   page = await Page.build();
@@ -12,10 +11,32 @@ afterEach( async () => {
   await page.close();
 });
 
+//for nest different test
+describe('when logged in', async () => {
+  beforeEach( async () => {
+    await page.login();
+    await page.click('a.btn-floating')
+  });
 
-test('should when logged in can see blog create ', async () => {
-  await page.login();
-  await page.click('a.btn-floating');
-  const label = await page.getContentsOf('form label');
-  expect(label).toEqual('Blog Title');
-})
+  test('can see blog create form', async () => {
+    const label = await page.getContentsOf('form label');
+    expect(label).toEqual('Blog Title');
+  });
+
+  describe('And using invalid inputs', async () => {
+    beforeEach( async () => {
+      await page.click('form button');
+    });
+    test('should form show an error message', async () => {
+      const titleError = await page.getContentsOf('.title .red-text');
+      const contentError = await page.getContentsOf('.content .red-text');
+      expect(titleError).toEqual('You must provide a value');      
+      expect(contentError).toEqual('You must provide a value');      
+    });
+  });
+  
+});
+
+
+
+
