8dbbfe3b9f2da07970147c2427a1162e2211e2ed
mazdak <nazemi.works@gmail.com>
2019-07-03 20:08:12 -0700

    make DRY

--- starter/tests/header.test.js
+++ starter/tests/header.test.js
@@ -1,7 +1,23 @@
 
+// puppeteer for headless browser testing with chromiunm 
+// pupet => browser => tab(page);
+const puppeteer = require('puppeteer');
+
+let browser, page;
+beforeEach( async () => {
+  // browser obj page obj
+  browser = await puppeteer.launch({
+    headless: false
+  });
+  page = await browser.newPage();
+  await page.goto('localhost:3000');
+});
 
 
-test('should add two numbers', () => {
-  const sum = 3 + 1;
-  expect(sum).toEqual(3);
+
+//puppeteer make text send to chromioum it turn obj
+test('should we can launch browser', async () => {
+  //el => el.innerHTML is text
+  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+  expect(text).toEqual('Blogster');
 })
