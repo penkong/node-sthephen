bd492583debd0227c29b95d6db2800d5cfb70804
mazdak <nazemi.works@gmail.com>
2019-07-03 23:57:54 -0700

    test logout done

--- starter/tests/header.test.js
+++ starter/tests/header.test.js
@@ -39,6 +39,7 @@ test('should clicking login to auth flow', async () => {
   // we use fake session and deceive cookie session believe it
 });
 
+//test.only
 test('when signed in show logout button', () => {
   const id = '5453rsff4tjdsfjsdf4fs';
   const Buffer = require('safe-buffer').Buffer;
@@ -47,15 +48,28 @@ test('when signed in show logout button', () => {
       usr: id
     }
   };
+
   // buffer session obj to string ddsgdfgerhgty56y54yu657uy65y-fdsg43ygg43tg34tgy-4yt3465346t34t34
   const sessionString = Buffer
     .from(JSON.stringify(sessionObject))
     .toString('base64');
+
   //key grip to generate signature;
   const Keygrip = require('keygrip');
   const keys = require('../config/keys');
   const keygrip = new Keygrip([keys.cookieKey]);
   const sig = keygrip.sign('session=' + sessionString);
+
+  await page.setCookie({name: 'session', value: sessionString});
+  await page.setCookie({name: 'session.sig', value: sig});
+  //after set cookie we refresh page for change happen
+  await page.goto('localhost:3000');
+
+  // it cause let all up lines exec then exec below
+  await page.waitFor('a[href="/auth/logout"]');
+
+  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+  expect(text).toEqual('Logout');
 })
 
 
