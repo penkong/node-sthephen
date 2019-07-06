8270d14a67284d741290e416923ca864a85a65c0
mazdak <nazemi.works@gmail.com>
2019-07-06 09:24:03 -0700

    make img available done

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -6,7 +6,11 @@ class BlogShow extends Component {
   componentDidMount() {
     this.props.fetchBlog(this.props.match.params._id);
   }
-
+  renderImage = () => {
+    if(this.props.blog.imageUrl) {
+      return <img src={'https://s3-us-west-2.amazonaws.com/bucketname'+this.props.blog.imageUrl} />
+    }
+  }
   render() {
     if (!this.props.blog) {
       return '';
@@ -18,6 +22,7 @@ class BlogShow extends Component {
       <div>
         <h3>{title}</h3>
         <p>{content}</p>
+        {this.renderImage()}
       </div>
     );
   }
