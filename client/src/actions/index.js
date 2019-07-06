514acdb5f0c394387c76e120357da653034f7fa6
mazdak <nazemi.works@gmail.com>
2019-07-06 09:16:19 -0700

    add image to db and backend and define on get on client

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -14,13 +14,17 @@ export const handleToken = token => async dispatch => {
 };
 // file upload place.
 export const submitBlog = (values, file, history) => async dispatch => {
+  // take link from express and amazon here
   const uploadConfig = await axios.get('/api/upload');
   await axios.put(uploadConfig.data.url, file, {
     headers: {
       'Content-Type': file.type
     }
   });
-  const res = await axios.post('/api/blogs', values);
+  const res = await axios.post('/api/blogs', {
+    ...values,
+    imageUrl: uploadConfig.data.key
+  });
 
   history.push('/blogs');
   dispatch({ type: FETCH_BLOG, payload: res.data });
